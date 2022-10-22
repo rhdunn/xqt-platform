fa8f839254253d70f1fa14b541fe4a86cac39e34
Reece H. Dunn <msclrhd@gmail.com>
2022-10-22 21:25:23 +0100

    Support advancing characters in XmlCharReader.

--- src/xqt-platform-xml/commonMain/xqt/platform/xml/model/XmlCharReader.kt
+++ src/xqt-platform-xml/commonMain/xqt/platform/xml/model/XmlCharReader.kt
@@ -14,8 +14,9 @@ class XmlCharReader {
      */
     fun reset(buffer: CharSequence, startOffset: Int, bufferEndOffset: Int) {
         this.buffer = buffer
-        this.currentOffset = startOffset
+        this.nextOffset = startOffset
         this.bufferEndOffset = bufferEndOffset
+        advance()
     }
 
     /**
@@ -25,6 +26,30 @@ class XmlCharReader {
      * @param startOffset The offset of the first codepoint in the buffer to read from.
      */
     fun reset(buffer: CharSequence, startOffset: Int = 0): Unit = reset(buffer, startOffset, buffer.length)
+
+    /**
+     * Advance to the next XmlChar in the buffer.
+     */
+    fun advance() {
+        currentOffset = nextOffset
+        if (currentOffset == bufferEndOffset) {
+            currentChar = EndOfBuffer
+            return
+        }
+
+        val high = buffer[nextOffset]
+        if (high.code in HighSurrogate && nextOffset + 1 != bufferEndOffset) {
+            val low = buffer[nextOffset + 1]
+            if (low.code in LowSurrogate) {
+                currentChar = XmlChar(high, low)
+                nextOffset += 2
+                return
+            }
+        }
+
+        currentChar = XmlChar(high)
+        nextOffset += 1
+    }
 
     /**
      * The underlying UTF-16 character sequence.
@@ -43,4 +68,22 @@ class XmlCharReader {
      */
     var currentOffset: Int = 0
         private set
+
+    /**
+     * The value of the current XmlChar in the buffer.
+     */
+    var currentChar: XmlChar = EndOfBuffer
+        private set
+
+    private var nextOffset: Int = 0
+
+    companion object {
+        /**
+         * Represents the end of the character sequence.
+         */
+        val EndOfBuffer: XmlChar = XmlChar(-1)
+
+        private val HighSurrogate = 0xD800..0xDBFF
+        private val LowSurrogate = 0xDC00..0xDFFF
+    }
 }
--- src/xqt-platform-xml/commonTest/xqt/platform/test/xml/model/XmlCharReader.kt
+++ src/xqt-platform-xml/commonTest/xqt/platform/test/xml/model/XmlCharReader.kt
@@ -2,6 +2,7 @@
 package xqt.platform.test.xml.model
 
 import kotlinx.test.DisplayName
+import xqt.platform.xml.model.XmlChar
 import xqt.platform.xml.model.XmlCharReader
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -49,5 +50,194 @@ class AnXmlCharReaderCanBeInitialized {
         assertEquals("lorem ipsum dolor", reader.buffer)
         assertEquals(6, reader.currentOffset)
         assertEquals(11, reader.bufferEndOffset)
+    }
+}
+
+@DisplayName("An XmlCharReader can read XmlChar values")
+class AnXmlCharReaderCanReadXmlCharValues {
+    @Test
+    @DisplayName("from an empty buffer")
+    fun from_an_empty_buffer() {
+        val reader = XmlCharReader()
+        reader.reset("")
+
+        assertEquals(0, reader.currentOffset)
+        assertEquals(XmlCharReader.EndOfBuffer, reader.currentChar)
+
+        reader.advance()
+        assertEquals(0, reader.currentOffset)
+        assertEquals(XmlCharReader.EndOfBuffer, reader.currentChar)
+    }
+
+    @Test
+    @DisplayName("from a sequence of ASCII characters")
+    fun from_a_sequence_of_ascii_characters() {
+        val reader = XmlCharReader()
+        reader.reset("lorem")
+
+        assertEquals(0, reader.currentOffset)
+        assertEquals(XmlChar('l'), reader.currentChar)
+
+        reader.advance()
+        assertEquals(1, reader.currentOffset)
+        assertEquals(XmlChar('o'), reader.currentChar)
+
+        reader.advance()
+        assertEquals(2, reader.currentOffset)
+        assertEquals(XmlChar('r'), reader.currentChar)
+
+        reader.advance()
+        assertEquals(3, reader.currentOffset)
+        assertEquals(XmlChar('e'), reader.currentChar)
+
+        reader.advance()
+        assertEquals(4, reader.currentOffset)
+        assertEquals(XmlChar('m'), reader.currentChar)
+
+        reader.advance()
+        assertEquals(5, reader.currentOffset)
+        assertEquals(XmlCharReader.EndOfBuffer, reader.currentChar)
+
+        reader.advance()
+        assertEquals(5, reader.currentOffset)
+        assertEquals(XmlCharReader.EndOfBuffer, reader.currentChar)
+    }
+
+    @Test
+    @DisplayName("from a sequence of Basic Multilingual Plane characters")
+    fun from_a_sequence_of_basic_multilingual_plane_characters() {
+        val reader = XmlCharReader()
+        reader.reset("\u0301\u0303\u0304")
+
+        assertEquals(0, reader.currentOffset)
+        assertEquals(XmlChar(0x0301), reader.currentChar)
+
+        reader.advance()
+        assertEquals(1, reader.currentOffset)
+        assertEquals(XmlChar(0x0303), reader.currentChar)
+
+        reader.advance()
+        assertEquals(2, reader.currentOffset)
+        assertEquals(XmlChar(0x0304), reader.currentChar)
+
+        reader.advance()
+        assertEquals(3, reader.currentOffset)
+        assertEquals(XmlCharReader.EndOfBuffer, reader.currentChar)
+
+        reader.advance()
+        assertEquals(3, reader.currentOffset)
+        assertEquals(XmlCharReader.EndOfBuffer, reader.currentChar)
+    }
+
+    @Test
+    @DisplayName("from a sequence of Supplementary Multilingual Plane characters")
+    fun from_a_sequence_of_supplementary_multilingual_plane_characters() {
+        val reader = XmlCharReader()
+        reader.reset("\uD83D\uDE01\uD83D\uDE03\uD83D\uDE04")
+
+        assertEquals(0, reader.currentOffset)
+        assertEquals(XmlChar(0x1F601), reader.currentChar)
+
+        reader.advance()
+        assertEquals(2, reader.currentOffset)
+        assertEquals(XmlChar(0x1F603), reader.currentChar)
+
+        reader.advance()
+        assertEquals(4, reader.currentOffset)
+        assertEquals(XmlChar(0x1F604), reader.currentChar)
+
+        reader.advance()
+        assertEquals(6, reader.currentOffset)
+        assertEquals(XmlCharReader.EndOfBuffer, reader.currentChar)
+
+        reader.advance()
+        assertEquals(6, reader.currentOffset)
+        assertEquals(XmlCharReader.EndOfBuffer, reader.currentChar)
+    }
+
+    @Test
+    @DisplayName("from a sequence of high surrogate codepoints")
+    fun from_a_sequence_of_high_surrogate_codepoints() {
+        val reader = XmlCharReader()
+        reader.reset("\uD801\uD803\uD804")
+
+        assertEquals(0, reader.currentOffset)
+        assertEquals(XmlChar(0xD801), reader.currentChar)
+
+        reader.advance()
+        assertEquals(1, reader.currentOffset)
+        assertEquals(XmlChar(0xD803), reader.currentChar)
+
+        reader.advance()
+        assertEquals(2, reader.currentOffset)
+        assertEquals(XmlChar(0xD804), reader.currentChar)
+
+        reader.advance()
+        assertEquals(3, reader.currentOffset)
+        assertEquals(XmlCharReader.EndOfBuffer, reader.currentChar)
+
+        reader.advance()
+        assertEquals(3, reader.currentOffset)
+        assertEquals(XmlCharReader.EndOfBuffer, reader.currentChar)
+    }
+
+    @Test
+    @DisplayName("from a sequence of low surrogate codepoints")
+    fun from_a_sequence_of_low_surrogate_codepoints() {
+        val reader = XmlCharReader()
+        reader.reset("\uDC01\uDC03\uDC04")
+
+        assertEquals(0, reader.currentOffset)
+        assertEquals(XmlChar(0xDC01), reader.currentChar)
+
+        reader.advance()
+        assertEquals(1, reader.currentOffset)
+        assertEquals(XmlChar(0xDC03), reader.currentChar)
+
+        reader.advance()
+        assertEquals(2, reader.currentOffset)
+        assertEquals(XmlChar(0xDC04), reader.currentChar)
+
+        reader.advance()
+        assertEquals(3, reader.currentOffset)
+        assertEquals(XmlCharReader.EndOfBuffer, reader.currentChar)
+
+        reader.advance()
+        assertEquals(3, reader.currentOffset)
+        assertEquals(XmlCharReader.EndOfBuffer, reader.currentChar)
+    }
+
+    @Test
+    @DisplayName("from a given range")
+    fun from_a_given_range() {
+        val reader = XmlCharReader()
+        reader.reset("lorem ipsum dolor", 6, 11)
+
+        assertEquals(6, reader.currentOffset)
+        assertEquals(XmlChar('i'), reader.currentChar)
+
+        reader.advance()
+        assertEquals(7, reader.currentOffset)
+        assertEquals(XmlChar('p'), reader.currentChar)
+
+        reader.advance()
+        assertEquals(8, reader.currentOffset)
+        assertEquals(XmlChar('s'), reader.currentChar)
+
+        reader.advance()
+        assertEquals(9, reader.currentOffset)
+        assertEquals(XmlChar('u'), reader.currentChar)
+
+        reader.advance()
+        assertEquals(10, reader.currentOffset)
+        assertEquals(XmlChar('m'), reader.currentChar)
+
+        reader.advance()
+        assertEquals(11, reader.currentOffset)
+        assertEquals(XmlCharReader.EndOfBuffer, reader.currentChar)
+
+        reader.advance()
+        assertEquals(11, reader.currentOffset)
+        assertEquals(XmlCharReader.EndOfBuffer, reader.currentChar)
     }
 }
