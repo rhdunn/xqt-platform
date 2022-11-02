5ec215cfcc951e13605d9b292b9120faad3f7c89
Reece H. Dunn <msclrhd@gmail.com>
2022-11-02 13:00:11 +0000

    Add an NCNameTokenType interface.

---
+++ src/xqt-platform-xml/commonMain/xqt/platform/xml/lexer/tokens/NCNameTokenType.kt
@@ -0,0 +1,9 @@
+// Copyright (C) 2022 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
+package xqt.platform.xml.lexer.tokens
+
+/**
+ * An NCName token in an XQT language.
+ *
+ * This can also be a keyword as keywords are valid NCNames.
+ */
+interface NCNameTokenType : TerminalSymbolTokenType
