a9077af9644c729f39d0079371a5d338c428dc93
Reece H. Dunn <msclrhd@gmail.com>
2022-11-06 17:21:25 +0000

    Add a W3CEditorsDraft annotation class.

--- src/xqt-platform-xml/commonMain/xqt/platform/xml/W3CProcess.kt
+++ src/xqt-platform-xml/commonMain/xqt/platform/xml/W3CProcess.kt
@@ -2,6 +2,29 @@
 package xqt.platform.xml
 
 /**
+ * This annotation marks the annotation as implementing a W3C Editor's Draft
+ * specification.
+ *
+ * Because the specification is a draft, the corresponding API is considered
+ * experimental and as such the behavior of such API may be changed or the
+ * API may be removed completely in any further release.
+ *
+ * > Beware using the annotated API especially if you're developing a library,
+ * > since your library might become binary incompatible with the future
+ * > versions of this library.
+ *
+ * Any usage of a declaration annotated with `@W3CEditorsDraft` must be accepted
+ * either by annotating that usage with the [OptIn] annotation, e.g.
+ * `@OptIn(W3CEditorsDraft::class)`, or by using the compiler argument
+ * `-opt-in=xqt.platform.xml.W3CEditorsDraft`.
+ */
+@RequiresOptIn(level = RequiresOptIn.Level.ERROR)
+@Retention(AnnotationRetention.BINARY)
+@Target(AnnotationTarget.CLASS)
+@MustBeDocumented
+annotation class W3CEditorsDraft
+
+/**
  * This annotation marks the annotation as implementing a W3C Recommendation
  * specification.
  */
