37c28606d08b2da6235e301906a81034e320bfe7
Reece H. Dunn <msclrhd@gmail.com>
2023-04-24 08:58:19 +0100

    Create a javadoc jar for each maven package.

--- src/xqt-platform-xml/build.gradle.kts
+++ src/xqt-platform-xml/build.gradle.kts
@@ -122,4 +122,13 @@ tasks.withType<DokkaTask>().configureEach {
     }
 }
 
+@Suppress("KDocMissingDocumentation")
+val javadocJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("javadoc")
+}
+
+publishing.publications.withType<MavenPublication> {
+    artifact(javadocJar)
+}
+
 // endregion
